8c0f57ef141a025b27fb5662b0d1b46baaddb63e
Matt Mansour <slackbabbath@gmail.com>
2013-04-23 07:49:35 -0700

    Adding causes to home page

--- cause/views.py
+++ cause/views.py
@@ -5,8 +5,9 @@ from cause.models import CausePage
 
 
 def home(request):
+    most_recent = CausePage.objects.filter(status=2).order_by('-publish_date')[:10]
     return render_to_response('index.html',
-                              {},
+                              {"most_recent":most_recent},
                               context_instance=RequestContext(request))
 
 
